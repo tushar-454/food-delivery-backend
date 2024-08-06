578137c0094fc517074cb600fed8a4dd06b7893b
tushar-454 <imtushar454@gmail.com>
2024-08-07 00:45:34 +0600

    feat: add verify user middleware

---
+++ src/middleware/verifyUser.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { getUserByProperty } = require('../services/v1/user');
+
+const verifyUser = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userExists = await getUserByProperty('email', decoded.email);
+    if (!userExists) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    req.user = userExists;
+    return next();
+  } catch (error) {
+    next(error);
+  }
+  return null;
+};
+
+module.exports = verifyUser;
