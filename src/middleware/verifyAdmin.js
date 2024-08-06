2a5a6fd5323120867567c66a15615d2bc9823aea
tushar-454 <imtushar454@gmail.com>
2024-08-07 00:48:19 +0600

    feat: add verify admin middleware

---
+++ src/middleware/verifyAdmin.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { getUserByProperty } = require('../services/v1/user');
+
+const verifyUser = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userExists = await getUserByProperty('email', decoded.email);
+    if (!userExists) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    if (userExists.role !== 'admin') {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    req.user = userExists;
+    return next();
+  } catch (error) {
+    next(error);
+  }
+  return null;
+};
+
+module.exports = verifyUser;
