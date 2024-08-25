7a4262ca8f05523a823e33315c4dd084d4f52309
tushar-454 <imtushar454@gmail.com>
2024-08-26 02:23:42 +0600

    refactor: Update payment routes to include user verification for making payments

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,10 +4,11 @@ const adminRoutes = require('./admin');
 const tokenRoutes = require('./token');
 const paymentRoutes = require('./payment');
 const verifyAdmin = require('../../middleware/verifyAdmin');
+const verifyUser = require('../../middleware/verifyUser');
 
 router.use('/api/v1/user', userRoutes);
 router.use('/api/v1/admin', verifyAdmin, adminRoutes);
 router.use('/api/v1/token', tokenRoutes);
-router.use('/api/v1/payment', paymentRoutes);
+router.use('/api/v1/payment', verifyUser, paymentRoutes);
 
 module.exports = router;
